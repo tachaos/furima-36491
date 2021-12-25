3245718ed68f497f7b807836aded66ad84515334
tachaos <89284825+tachaos@users.noreply.github.com>
2021-12-25 17:23:51 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   has_one :seller
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :delivery_charge
+    belongs_to :delivery_source
+    belongs_to :delivery_days
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_source_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

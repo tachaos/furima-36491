f4aaebb98f2436e34aadb8b776e8f4be4ecf7200
tachaos <89284825+tachaos@users.noreply.github.com>
2022-01-08 13:07:55 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,12 +17,12 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :category_id,        numericality: { other_than: 0 , message: "can't be blank"}
+  validates :status_id,          numericality: { other_than: 0 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 0 , message: "can't be blank"}
   validates :delivery_source_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :delivery_days_id, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
-  validates :price, numericality: { only_integer: true, message: "Half-width number." }
+  validates :delivery_days_id,   numericality: { other_than: 0 , message: "can't be blank"}
+  validates :price,              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :price,              numericality: { only_integer: true, message: "Half-width number." }
 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    name               {Faker::Name.name}
+    description        {"説明"}
+    price              {2000}
+    category_id        {1}
+    status_id          {1}
+    delivery_charge_id {1}
+    delivery_source_id {1}
+    delivery_days_id   {1}
+    association :user
   end
 end

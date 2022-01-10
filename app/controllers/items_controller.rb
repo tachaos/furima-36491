f1f9b8599961d0791a9847316c950aebd74a8693
tachaos <89284825+tachaos@users.noreply.github.com>
2022-01-10 14:48:39 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.save
-      render :show
+    if @item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end

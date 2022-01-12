af61620c3aa09f8c10b6784f23cb1b7f4b2b63a0
tachaos <89284825+tachaos@users.noreply.github.com>
2022-01-13 00:11:34 +0900

    formオブジェクトまで

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,6 +1,26 @@
 class SellersController < ApplicationController
-  
+  before_action :authenticate_user!, except: :index
+
   def index
+    @item = Item.find(params[:item_id])
+    @seller_destination = SellerDestination.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @seller_destination = SellerDestination.new(seller_params)
+    if @seller_destination.valid?
+      @seller_destination.save
+      redirect_to root_path 
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def seller_params
+    params.require(:seller_destination).permit(:postal_code, :delivery_source_id, :city, :block_number, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 
 end
